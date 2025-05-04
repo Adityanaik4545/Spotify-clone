d1d5ff1dbe7356b56171af1bb5c5a4c25266d524
Adityanaik4545 <adityank23@outlook.com>
2025-05-04 23:34:27 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,39 @@
+async function getSongs() {
+    let a= await fetch("http://127.0.0.1:5500/song/");
+    let response= await a.text();
+    let div=document.createElement("div");
+    div.innerHTML=response;
+    let as= div.getElementsByTagName("a");
+    songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/song/")[1])
+        }
+    }
+    return songs;
+    
+}
+async function main() {
+    let songs= await getSongs()
+    console.log(songs);
+    let songUl=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUl.innerHTML=songUl.innerHTML+`<li><img class="invert" src="image/music.svg" alt="">
+                        <div class="info">
+                            <div>${song.replaceAll("%20","")}</div>
+                            <div>aditya</div>
+                        </div>
+                        <div class="playnow">
+                            <span>Play now</span>
+                            <img class="invert" src="image/play.svg" alt="">
+                        </div></li>`;
+    }
+    var audio = new Audio(songs[0]);
+audio.play();
+audio.addEventListener("loadeddata",()=>{
+    console.log(audio.duration,audio.currentSrc,audio.currentTime);
+    
+})
+}
+main()
